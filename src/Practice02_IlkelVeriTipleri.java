1b75ee2bc9ceef178e2a0dc74860769af2da5d6d
IlknurDoganalp <128212123+IlknurDoganalp@users.noreply.github.com>
2024-12-18 12:56:04 +0300

    JavaPractice1

---
+++ src/Practice02_IlkelVeriTipleri.java
@@ -0,0 +1,22 @@
+public class Practice02_IlkelVeriTipleri {
+    public static void main(String[] args) {
+
+        int sayi1 = 10; //int pozitif, negatif veya sifir olabilir. Bellekte 4 byte (32bit)'e kadar yer kaplar.
+        long sayi2 = 10000000000L;
+        float sayi3 = 3.14f;
+        double sayi4 = 2.71828;
+
+        char karakter = 'A';
+        boolean trueOrFalse = true;
+
+        // String (String)
+        String metin = "Hello World";
+
+        System.out.println(sayi1);
+        System.out.println(sayi2);
+        System.out.println(sayi3);
+        System.out.println(sayi4);
+        System.out.println(karakter);
+        System.out.println(trueOrFalse);
+    }
+}
